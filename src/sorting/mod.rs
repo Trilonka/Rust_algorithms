3726f4d83ab3cef7b26f184f0ecbcf9195e7a46a
Trilonka <aniskov.stepan@mail.ru>
2022-07-05 15:54:22 +0300

    add selection_sort

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -7,6 +7,7 @@ pub mod gnome_sort;
 pub mod odd_even_sort;
 pub mod merge_sort;
 pub mod pancake_sort;
+pub mod selection_sort;
 
 pub use self::bubble_sort::bubble_sort;
 pub use self::cocktail_shaker_sort::cocktail_shaker_sort;
@@ -16,4 +17,5 @@ pub use self::insertion_sort::insertion_sort;
 pub use self::gnome_sort::gnome_sort;
 pub use self::odd_even_sort::odd_even_sort;
 pub use self::merge_sort::merge_sort;
-pub use  self::pancake_sort::pancake_sort;
+pub use self::pancake_sort::pancake_sort;
+pub use self:: selection_sort::selection_sort;
---
+++ src/sorting/selection_sort.rs
@@ -0,0 +1,59 @@
+pub fn selection_sort<T: PartialOrd>(arr: &mut [T]) {
+    let len = arr.len();
+    for left in 0..len {
+        let mut smallest = left;
+        for right in (left+1)..len {
+            if arr[right] < arr[smallest] {
+                smallest = right;
+            }
+        }
+        arr.swap(left, smallest);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let mut res = vec![10, 8, 4, 3, 1, 9, 2, 7, 5, 6];
+        selection_sort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn basic_string() {
+        let mut res = vec!["a", "bb", "d", "cc"];
+        selection_sort(&mut res);
+        assert_eq!(res, vec!["a", "bb", "cc", "d"]);
+    }
+
+    #[test]
+    fn empty() {
+        let mut res = Vec::<u8>::new();
+        selection_sort(&mut res);
+        assert_eq!(res, vec![]);
+    }
+
+    #[test]
+    fn one_element() {
+        let mut res = vec![1];
+        selection_sort(&mut res);
+        assert_eq!(res, vec![1]);
+    }
+
+    #[test]
+    fn pre_sorted() {
+        let mut res = vec![1, 2, 3, 4];
+        selection_sort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn reverse_sorted() {
+        let mut res = vec![4, 3, 2, 1];
+        selection_sort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+}
