5948a5fac90036ab05c39e73494fdfcb997f2056
Trilonka <aniskov.stepan@mail.ru>
2022-07-10 21:17:18 +0300

    add exponential search

--- src/searching/mod.rs
+++ src/searching/mod.rs
@@ -1,5 +1,7 @@
 pub mod linear_search;
 pub mod binary_search;
+pub mod exponential_search;
 
 pub use self::linear_search::linear_search;
 pub use self::binary_search::binary_search;
+pub use self::exponential_search::exponential_search;
